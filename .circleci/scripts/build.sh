4c341a47ed2c45cd04570b9b1bcd39acad76df7c
vabd <vabd@informo.network>
2019-04-26 20:51:27 +0100

    Add logging

--- .circleci/scripts/build.sh
+++ .circleci/scripts/build.sh
@@ -2,10 +2,14 @@
 
 .circleci/scripts/check_open_scs.sh
 
+pr_number=`.circleci/scripts/pr_number.sh`
+
 if [ "$?" == "1" ]; then
     path_prefix="pr"
 else
     path_prefix="scs"
 fi
 
-hugo -v -b /$path_prefix/`.circleci/scripts/pr_number.sh`/ -d workspace/public
+printf "Building %s #%d" $path_prefix $pr_number
+
+hugo -v -b /$path_prefix/$pr_number/ -d workspace/public
