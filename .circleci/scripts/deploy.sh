fd87586d7ad0b939d6bb780e081cf766af27a38d
vabd <vabd@informo.network>
2019-04-26 19:29:45 +0100

    Add support for forced deployment of non-SCS PRs

--- .circleci/scripts/deploy.sh
+++ .circleci/scripts/deploy.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+deploy() {
+	ssh http@it-01.informo.network mkdir -p /srv/http/specs/public/$1/$2 2>&1 > /dev/null
+	scp -r /tmp/workspace/public/* http@it-01.informo.network:/srv/http/specs/public/$1/$2/
+}
+
 PR_NUMBER=`/tmp/workspace/scripts/pr_number.sh`
 
 /tmp/workspace/scripts/check_open_scs.sh $PR_NUMBER
 
 if [ "$?" == "0" ]; then
-	ssh http@it-01.informo.network mkdir -p /srv/http/specs/public/scs/$PR_NUMBER 2>&1 > /dev/null
-	scp -r /tmp/workspace/public/* http@it-01.informo.network:/srv/http/specs/public/scs/$PR_NUMBER/
+	deploy "scs" $PR_NUMBER
+elif [ "$?" == "1" ]; then
+	deploy "pr" $PR_NUMBER
 else
 	echo "This is not an open SCS, aborting deployment"
 fi
