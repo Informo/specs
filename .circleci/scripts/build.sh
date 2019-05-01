0cbc769858852a23ee1c4b401b8443a65a1b83e8
vabd <34184120+vabd@users.noreply.github.com>
2019-05-01 22:34:58 +0100

    Add support for forced deployment of non-SCS PRs (#42)
    * Add support for forced deployment of non-SCS PRs
    
    * Move build to script
    
    * chmod +x
    
    * Add logging
    
    * Fix call order
    
    * Fix call
    
    * Fix stuff
    
    * Add new exit code
    
    * Build needs jq now
    
    * Add some more logging while we're at it

---
+++ .circleci/scripts/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+pr_number=`.circleci/scripts/pr_number.sh`
+
+.circleci/scripts/check_open_scs.sh $pr_number
+
+ret=$?
+
+echo "check_open_scs.sh exited with code $ret"
+
+if [ "$ret" == "1" ]; then
+    path_prefix="pr"
+else
+    path_prefix="scs"
+fi
+
+printf "Building %s #%d\n" $path_prefix $pr_number
+
+hugo -v -b /$path_prefix/$pr_number/ -d workspace/public
